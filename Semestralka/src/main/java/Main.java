afb8412035b9bad175a90b8cfa06b0aa1c76f78c
Matúš <mymail@gmail.com>
2024-12-21 17:23:41 +0100

    edited main

--- Semestralka/src/main/java/Main.java
+++ Semestralka/src/main/java/Main.java
@@ -1,10 +1,6 @@
-import shapes.*;
-import tvary.Platno;
 
 public class Main {
 
     public static void main(String[] args) {
-        Platno.dajPlatno();
-        ShapeWeird s = new ShapeWeird(100, 100);
     }
 }
